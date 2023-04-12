83b85b5236f59fdc44bd174cdecdde3e0064e873
Dafahan <dafahanid@gmail.com>
2023-04-12 23:01:49 +0700

    Create template.cpp

---
+++ template.cpp
@@ -0,0 +1,29 @@
+#include<bits/stdc++.h>
+#define fastio ios::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+#define ll long long
+#define FOR(a,b,c) for(int a=b;c;a++)
+#define RFOR(a,b,c) for(int a=b;c;a--)
+#define all(a) a.begin(),a.end()
+#define sz size()
+#define elif else if
+#define pll pair<ll,ll>
+#define pii pair<int,int>
+#define fi first
+#define se second
+#define pb push_back
+#define br cout<<endl;
+using namespace std;
+
+int n,k;
+string s;
+signed main(){
+    char c;
+    int cnt=0;
+    while(cin>>c){
+        cnt++;
+        cout<<"    \""<<c<<"\", // "<<cnt;
+        br
+    }
+   return 0;
+}
+
