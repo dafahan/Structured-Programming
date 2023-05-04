07a9b3c9ea755133af5e517a40cd04354f59c119
Dafahan <dafahanid@gmail.com>
2023-05-04 15:12:18 +0700

    Update stack.py

--- stack.py
+++ stack.py
@@ -19,6 +19,8 @@ class Stack:
 
     def size(self):
         return len(self.stack)
+    def rm(self,item):
+        del self.stack[item]
 
     def remove(self, item):
         if item in self.stack:
@@ -26,3 +28,17 @@ class Stack:
             return item
         else:
             return None
+s = Stack() #deklarasi stac
+
+s.push(1) #memasukan nilai
+s.push(2)
+s.push(3)
+s.push(4)
+s.push(5)
+s.push(6)
+s.remove(s.peek())#hapus nilai paling atas
+s.rm(1) #hapus stack ke 1
+
+while(s.size()):
+    print(s.peek())
+    s.remove(s.peek())
